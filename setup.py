3e98f5d248b16fe10459a0dbada5a7de79130cda
esteban vilca <esteban.wilfredo.g@gmail.com>
2022-02-26 16:56:46 +0000

    fixing error link github

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setuptools.setup(
        "numpy>=1.21.5",
        "pandas>=1.3.4",
        "scipy>=1.7.3",
-       "etalib @ git+ssh://git@github.com/estebanvz/eta-lib.git"
+       "etalib @ git+https://github.com/estebanvz/crypto_metrics"
    ],
 )
